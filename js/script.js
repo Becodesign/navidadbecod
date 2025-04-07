754b6f39d6d6fa46df355913fde6550376144391
Becodesign <materialhyo@gmail.com>
2025-04-07 11:43:57 -0500

    expand

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+document.getElementById("button-max-min").addEventListener("click", () => {
+    const button = document.getElementById("button-max-min");
+    const icon = button.querySelector("i");
+
+    if (!document.fullscreenElement) {
+        // Activar pantalla completa
+        document.documentElement.requestFullscreen();
+        icon.classList.remove("fa-expand");
+        icon.classList.add("fa-minimize");
+    } else {
+        // Salir de pantalla completa
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+            icon.classList.remove("fa-minimize");
+            icon.classList.add("fa-expand");
+        }
+    }
+});
